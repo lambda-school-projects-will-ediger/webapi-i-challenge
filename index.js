26a0165a15952378f616bd45a0ff8bf4a66dc93b
unknown <will.ediger@gmail.com>
2019-07-24 15:43:55 -0500

    implemented GET methods

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// implement your API here
+//user schema
+// {
+//   name: "Jane Doe", // String, required
+//   bio: "Not Tarzan's Wife, another Jane",  // String
+//   created_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
+//   updated_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
+// }
+
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(friend => {
+      if (friend) {
+        res.status(200).json(friend);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+server.listen(4000, () => {
+  console.log("server listening on port 4000");
+});
